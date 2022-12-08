9f95bcf391b3fa074a5ed7185d266983259369ee
yusheng-zhu12 <82942007+yusheng-zhu12@users.noreply.github.com>
2022-12-08 11:27:01 +0800

    Add files via upload

---
+++ Needle/Needle/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Needle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.init = new System.Windows.Forms.Button();
+            this.Launch = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.timerRotate = new System.Windows.Forms.Timer(this.components);
+            this.timerLaunch = new System.Windows.Forms.Timer(this.components);
+            this.roomhost = new System.Windows.Forms.Button();
+            this.gamer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(598, 593);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // init
+            // 
+            this.init.Location = new System.Drawing.Point(24, 643);
+            this.init.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.init.Name = "init";
+            this.init.Size = new System.Drawing.Size(84, 28);
+            this.init.TabIndex = 2;
+            this.init.Text = "init";
+            this.init.UseVisualStyleBackColor = true;
+            this.init.Click += new System.EventHandler(this.init_Click);
+            // 
+            // Launch
+            // 
+            this.Launch.Location = new System.Drawing.Point(290, 643);
+            this.Launch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Launch.Name = "Launch";
+            this.Launch.Size = new System.Drawing.Size(84, 28);
+            this.Launch.TabIndex = 3;
+            this.Launch.Text = "Launch";
+            this.Launch.UseVisualStyleBackColor = true;
+            this.Launch.Click += new System.EventHandler(this.Launch_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(425, 643);
+            this.Stop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(84, 28);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(156, 643);
+            this.Start.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(84, 28);
+            this.Start.TabIndex = 5;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // timerRotate
+            // 
+            this.timerRotate.Tick += new System.EventHandler(this.timerRotate_Tick);
+            // 
+            // timerLaunch
+            // 
+            this.timerLaunch.Tick += new System.EventHandler(this.timerLaunch_Tick);
+            // 
+            // roomhost
+            // 
+            this.roomhost.Location = new System.Drawing.Point(537, 614);
+            this.roomhost.Name = "roomhost";
+            this.roomhost.Size = new System.Drawing.Size(75, 30);
+            this.roomhost.TabIndex = 6;
+            this.roomhost.Text = "host";
+            this.roomhost.UseVisualStyleBackColor = true;
+            this.roomhost.Click += new System.EventHandler(this.roomhost_Click);
+            // 
+            // gamer
+            // 
+            this.gamer.Location = new System.Drawing.Point(537, 651);
+            this.gamer.Name = "gamer";
+            this.gamer.Size = new System.Drawing.Size(75, 31);
+            this.gamer.TabIndex = 7;
+            this.gamer.Text = "gamer";
+            this.gamer.UseVisualStyleBackColor = true;
+            this.gamer.Click += new System.EventHandler(this.gamer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 685);
+            this.Controls.Add(this.gamer);
+            this.Controls.Add(this.roomhost);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Launch);
+            this.Controls.Add(this.init);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "主体";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button init;
+        private System.Windows.Forms.Button Launch;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Timer timerRotate;
+        private System.Windows.Forms.Timer timerLaunch;
+        private System.Windows.Forms.Button roomhost;
+        private System.Windows.Forms.Button gamer;
+    }
+}
+
---
+++ Needle/Needle/Form1.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Needle
+{
+    public partial class Form1 : Form
+    {
+
+        //********************//
+        private static Socket ServerSocket;
+        private static Socket socketAccept;
+        CancellationTokenSource cancellationTokenSource;
+        ManualResetEvent manualReset = new ManualResetEvent(true);
+        public Socket client1 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        public Socket client2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        public Socket client3 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        public Socket client_receive;
+        bool host = true;
+        public int point = 1;
+        public int your_point = 1;
+        public int person_point = 1;
+        private bool flag = true;
+        //********************//
+
+        int INTERVAL_LAUNCH = 10;
+        int INTERVAL_ROTATE = 30;
+        Bitmap bitmap;
+
+        float LineWidth;//线宽
+
+        float cx, cy;//圆心
+        float offset_y;
+        float r;//半径
+
+        float Length;//线长
+
+        float speed_launch = 10;//发射速度
+        float speed_rotate = 5;//旋转速度
+
+        Matrix matrix;
+        PointF rotate_center;
+
+        GraphicsPath graphicsPath = new GraphicsPath();//在圆上的针
+
+        //CancellationTokenSource cancellationTokenSource;
+        //CancellationToken cancellationToken;
+        //ManualResetEvent resetEvent = new ManualResetEvent(true);
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+
+        }
+
+        private void Launch_Click(object sender, EventArgs e)
+        {
+            //***************//
+            byte[] msg = null;
+            string strmsg = "lauch";
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Broadcast, 5000);
+            msg = System.Text.Encoding.UTF8.GetBytes(strmsg);
+            client2.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+            client2.SendTo(msg, iPEndPoint);//发送数据
+            //***************//
+            //Task.Run(() => LaunchLine());
+        }
+
+        private void LaunchLine()
+        {
+            Pen penbk = new Pen(Color.White, LineWidth);
+            Pen pen1 = new Pen(Color.Black, LineWidth);
+            float x1 = pictureBox1.Width / 2;
+            float y1 = pictureBox1.Height;
+            float x2 = pictureBox1.Width / 2;
+            float y2 = pictureBox1.Height - Length;
+
+            try
+            {
+                while (y2 > cy + r + LineWidth * 5)
+                {
+
+                    Thread.Sleep(INTERVAL_LAUNCH);
+                    //Bitmap b = bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), bitmap.PixelFormat);
+                    Graphics graphics = Graphics.FromImage(bitmap);
+                    graphics.DrawLine(penbk, x1, y1, x2, y2);
+                    y1 -= speed_launch;
+                    y2 -= speed_launch;
+
+                    var color = bitmap.GetPixel((int)x2 + 5, (int)(y2 - 5));
+                    //var color = b.GetPixel((int)x2 + 5, (int)(y2 - 5));
+                    int c1 = color.ToArgb();
+                    int c2 = Color.Black.ToArgb();
+                    int c3 = Color.White.ToArgb();
+                    if (c1 == c2)
+                    {
+                        timerRotate.Stop();
+                        return;
+                    }
+
+                    graphics.DrawLine(pen1, x1, y1, x2, y2);
+                    //bitmap = b;
+                    pictureBox1.Image = bitmap;
+                }
+                graphicsPath.AddLine(new PointF(x1, y1), new PointF(x2, y2));
+                graphicsPath.CloseFigure();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            timerRotate.Stop();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            //*******************************//
+            if(host)
+            {
+                Start.Enabled = false;
+                IPEndPoint iPEndPoint1 = new IPEndPoint(IPAddress.Any, 5000);
+                client_receive = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                client_receive.Bind(iPEndPoint1);
+                cancellationTokenSource = new CancellationTokenSource();
+                CancellationToken cancellationToken = cancellationTokenSource.Token;
+                manualReset.Set();
+                Task.Run(() => Receive(client_receive, cancellationToken), cancellationToken);
+
+                IPEndPoint iPEndPoint2 = new IPEndPoint(IPAddress.Broadcast, 5000);
+                client2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                byte[] msg = null;
+                string strmsg = "start";
+                msg = System.Text.Encoding.UTF8.GetBytes(strmsg);
+                client2.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+                client2.SendTo(msg, iPEndPoint2);//发送数据
+            }
+            //*******************************//
+        }
+
+        private void timerLaunch_Tick(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        private void timerRotate_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Pen penbk = new Pen(Color.White, LineWidth);
+                Pen pen1 = new Pen(Color.Black, LineWidth);
+                //Bitmap b = bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), bitmap.PixelFormat);
+                Graphics graphics = Graphics.FromImage(bitmap);
+                graphics.DrawPath(penbk, graphicsPath);
+                graphicsPath.Transform(matrix);
+                graphics.DrawPath(pen1, graphicsPath);
+                //bitmap = b;
+                pictureBox1.Image = bitmap;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void init_Click(object sender, EventArgs e)
+        {
+            //设置 颜色、线
+            LineWidth = 8f;
+            Length = 50f;
+            Pen penbk = new Pen(Color.White, LineWidth);
+            Pen pen1 = new Pen(Color.Black, LineWidth);
+
+            timerLaunch.Interval = INTERVAL_LAUNCH;
+            timerRotate.Interval = INTERVAL_ROTATE;
+
+            //背景初始化
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.White);
+
+            //画圆
+            r = 40f;
+            offset_y = -10f;
+            cx = pictureBox1.Width / 2;
+            cy = pictureBox1.Height / 2 + offset_y;
+            SolidBrush brush = new SolidBrush(Color.Black);
+            graphics.FillEllipse(brush, cx - r, cy - r, r * 2, r * 2);
+
+            matrix = new Matrix();
+            rotate_center = new PointF(cx, cy);
+            matrix.RotateAt(speed_rotate, rotate_center);
+
+            //画线
+            PointF pt1 = new PointF(pictureBox1.Width / 2, pictureBox1.Height);
+            PointF pt2 = new PointF(pictureBox1.Width / 2, pictureBox1.Height - Length);
+            graphics.DrawLine(pen1, pt1, pt2);
+            pictureBox1.Image = bitmap;
+            //***************//
+            if (host)
+            {
+                cancellationTokenSource = new CancellationTokenSource();
+                CancellationToken cancellationToken = cancellationTokenSource.Token;
+                manualReset.Set();
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 6000);
+                client1 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                client1.Bind(iPEndPoint);
+                Task.Run(() => Receive_initial(client1, cancellationToken), cancellationToken);
+            }
+            else
+            {
+                cancellationTokenSource = new CancellationTokenSource();
+                CancellationToken cancellationToken = cancellationTokenSource.Token;
+                manualReset.Set();
+                IPEndPoint iPEndPoint1 = new IPEndPoint(IPAddress.Any, 6000);
+                client1 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                client1.Bind(iPEndPoint1);
+                Task.Run(() => Send_initial(client1));
+                IPEndPoint iPEndPoint2 = new IPEndPoint(IPAddress.Any, 5000);
+                client2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                client2.Bind(iPEndPoint2);
+                Task.Run(() => Receive(client2, cancellationToken), cancellationToken);
+                IPEndPoint iPEndPoint3 = new IPEndPoint(IPAddress.Any, 7000);
+                client3.Bind(iPEndPoint3);
+                Task.Run(() => Receive_initial(client3, cancellationToken), cancellationToken);
+            }
+            //***************//
+        }
+
+        //**********************************//
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Send_initial(Socket socket)
+        {
+            byte[] msg = null;
+            string strmsg = "join";
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Broadcast, 6000);
+            msg = System.Text.Encoding.UTF8.GetBytes(strmsg);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+            socket.SendTo(msg, iPEndPoint);//发送数据
+        }
+
+        private void Receive_initial(Socket socket, CancellationToken cancellationToken)
+        {
+            if(host)
+            {
+                while (true)
+                {
+
+                    if (cancellationToken.IsCancellationRequested)//是否任务已被取消
+                    {
+                        break;
+                    }
+
+                    manualReset.WaitOne();//暂停/恢复线程
+
+                    if (cancellationToken.IsCancellationRequested)//是否任务已被取消
+                    {
+                        break;
+                    }
+
+                    // Creates an IPEndPoint to capture the identity of the sending host.
+                    IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+                    EndPoint senderRemote = (EndPoint)sender;
+
+                    byte[] msg = new byte[1024];//接收数据
+                    try
+                    {
+                        socket.ReceiveTimeout = 500;//设置等待时长
+                        socket.ReceiveFrom(msg, ref senderRemote);// This call blocks for 500ms
+                        string strmsg = System.Text.Encoding.UTF8.GetString(msg);
+                        strmsg = strmsg.Replace("\0", "");
+                        if (strmsg == "join")
+                        {
+                            person_point++;
+                            byte[] msg1 = null;
+                            byte[] msg2 = null;
+                            string strmsg1 = person_point.ToString();
+                            string strmsg2 = strmsg1;
+                            IPEndPoint iPEndPoint1 = new IPEndPoint(IPAddress.Broadcast, 7000);
+                            msg1 = System.Text.Encoding.UTF8.GetBytes(strmsg1);
+                            msg2 = System.Text.Encoding.UTF8.GetBytes(strmsg2);
+                            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+                            socket.SendTo(msg1, iPEndPoint1);//发送数据
+                            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+                            socket.SendTo(msg2, iPEndPoint1);//发送数据
+                        }
+                    }
+                    catch (SocketException e1)
+                    {
+                        //MessageBox.Show(e1.ToString());//超时异常
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                        break;
+                    }
+                }
+
+            }
+            else
+            {
+                while (true)
+                {
+                    IPEndPoint sender = new IPEndPoint(IPAddress.Any, 7000);
+                    EndPoint senderRemote = (EndPoint)sender;
+                    byte[] msg = new byte[1024];//接收数据
+                    try
+                    {
+                        socket.ReceiveTimeout = 500;//设置等待时长
+                        socket.ReceiveFrom(msg, ref senderRemote);// This call blocks for 500ms
+                        string strmsg1 = System.Text.Encoding.UTF8.GetString(msg);
+                        socket.ReceiveFrom(msg, ref senderRemote);// This call blocks for 500ms
+                        string strmsg2 = System.Text.Encoding.UTF8.GetString(msg);
+                        if (flag)
+                        {
+                            your_point = int.Parse(strmsg1);
+                            flag = false;
+                        }
+                        person_point = int.Parse(strmsg2);
+                        MessageBox.Show("Receive_initial");
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+
+        }
+
+        private void roomhost_Click(object sender, EventArgs e)
+        {
+            host = true;
+        }
+
+        private void gamer_Click(object sender, EventArgs e)
+        {
+            host = false;
+            Start.Enabled = false;
+        }
+
+        private void Receive(Socket socket, CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+
+                if (cancellationToken.IsCancellationRequested)//是否任务已被取消
+                {
+                    break;
+                }
+
+                manualReset.WaitOne();//暂停/恢复线程
+
+                if (cancellationToken.IsCancellationRequested)//是否任务已被取消
+                {
+                    break;
+                }
+
+                // Creates an IPEndPoint to capture the identity of the sending host.
+                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 5000);
+                EndPoint senderRemote = (EndPoint)sender;
+
+                byte[] msg = new byte[1024];//接收数据
+                try
+                {
+                    socket.ReceiveTimeout = 500;//设置等待时长
+                    socket.ReceiveFrom(msg, ref senderRemote);// This call blocks for 500ms
+                    string strmsg = System.Text.Encoding.UTF8.GetString(msg);
+                    strmsg = strmsg.Replace("\0", "");
+                    if (strmsg == "start")
+                    {
+                        Start.Invoke((MethodInvoker)delegate { timerRotate.Start(); });
+                    }
+                    else if (strmsg == "lauch")
+                    {
+                        point++;
+                        if (point % person_point == your_point) 
+                            Launch.Enabled = true;
+                        else
+                        {
+                            if ((point % person_point == 0) && (your_point == person_point)) 
+                                Launch.Enabled = true;
+                            else
+                                Launch.Enabled = false;
+                        }
+                        Task.Run(() => LaunchLine());
+                    }
+                    else if (strmsg == "over")
+                    {
+
+                    }
+                }
+                catch (SocketException e1)
+                {
+                    //MessageBox.Show(e1.ToString());//超时异常
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
